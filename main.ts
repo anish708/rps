ba0a5878344f9ca89fa540b6c88686b0d12acde1
anish708 <122548189+anish708@users.noreply.github.com>
2023-01-31 11:20:22 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,9 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showString("A")
+    basic.clearScreen()
+    PA += 1
+    Rounds += 1
+})
 function showScoreboard () {
     OLED.clear()
     OLED.newLine()
@@ -8,16 +14,35 @@ function showScoreboard () {
     OLED.writeStringNewLine("Ties:" + Ties)
     OLED.newLine()
     OLED.writeStringNewLine("Rounds:" + Rounds)
+    reset()
 }
-let Rounds = 0
+input.onButtonPressed(Button.AB, function () {
+    basic.showString("T")
+    basic.clearScreen()
+    Ties += 1
+    Rounds += 1
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showString("B")
+    basic.clearScreen()
+    PB += 1
+    Rounds += 1
+})
+input.onGesture(Gesture.Shake, function () {
+    reset()
+})
+function reset () {
+    OLED.init(128, 64)
+    PA = 0
+    PB = 0
+    Ties = 0
+    Rounds = 0
+    OLED.writeStringNewLine("Let's play a game")
+    basic.pause(2000)
+    showScoreboard()
+}
 let Ties = 0
 let PB = 0
+let Rounds = 0
 let PA = 0
-OLED.init(128, 64)
-PA = 0
-PB = 0
-Ties = 0
-Rounds = 0
-OLED.writeStringNewLine("Let's play a game")
-basic.pause(2000)
-showScoreboard()
+reset()
